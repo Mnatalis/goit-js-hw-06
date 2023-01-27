6e6c933b5f5f8b01926e0281c5d08905cc37e772
Nataliss <oksklient@gmail.com>
2023-01-27 13:06:10 +0000

    Update-tasc 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const liItem = document.querySelectorAll(".item");
-console.log(`Number of categoris: ${liItem.childElementCount}`);
+const itemElement = document.querySelectorAll(".item");
+
+// console.log(`Number of categoris: ${liItem.childElementCount}`);
+
+console.log(`Number of categoris:`, itemElement.length);
+
+itemElement.forEach((item) => {
+    console.log(`Categoris:${item.firstElementChild.textContent}`);
+    console.log(`Elements:${item.lastElementChild.children.length}`);
+});
